a07891d036604596b7de9a7e0139c0432807a0cd
jdoerfler <james.c.doerfler@gmail.com>
2022-06-14 13:20:44 -0600

    changed import in datasets.__init__()

--- src/rai_toolbox/datasets/__init__.py
+++ src/rai_toolbox/datasets/__init__.py
@@ -5,7 +5,7 @@
 from ._imagenet_base import ImageNet, ImageNetM10, RestrictedImageNet
 from .cifar10_extensions import CIFAR10P1
 from .cifar_corruptions import CIFAR10C, CIFAR100C
-from .mnist_corruptions_kes import MNISTC
+from .mnist_corruptions import MNISTC
 
 # TODO: fix package errors and remove kes identifier in package file
 
